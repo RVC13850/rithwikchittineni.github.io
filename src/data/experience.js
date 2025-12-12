f76fa5c3174393cc172498354ccab02c6c414621
RVC13850 <rithwikchittineni@gmail.com>
2025-12-12 11:01:23 -0600

    chnages

--- src/data/experience.js
+++ src/data/experience.js
@@ -4,7 +4,7 @@ export const experience = [
       title: "Software Engineering Intern",
       company: "American Airlines",
       location: "Fort Worth, TX",
-      period: "June 2025 – August 2025",
+      period: "June 2025 – Aug 2025",
       description: "Developed and deployed internal automation tools to streamline data validation, reporting, and system monitoring, reducing manual effort by over 50%. Built and maintained backend services using Python, Flask, and PostgreSQL. Integrated CI/CD pipelines with GitHub Actions and Docker, accelerating development cycles and improving code reliability.",
       icon: <Rocket className="w-6 h-6" />
     },
@@ -12,7 +12,7 @@ export const experience = [
       title: "Software Engineer Intern",
       company: "Veritis Group Inc.",
       location: "Irving, TX",
-      period: "June 2024 – August 2024",
+      period: "June 2024 – Aug   2024",
       description: "Contributed to the creation of a robust Customer Portal application designed to efficiently track, store, and display project data. Collaborated on the design and implementation of a Java-based backend, ensuring data integrity and seamless functionality. Leveraged AWS services for application hosting, ensuring scalability and high availability.",
       icon: <Code className="w-6 h-6" />
     },
@@ -20,7 +20,7 @@ export const experience = [
       title: "Artificial Intelligence Mentee",
       company: "AI Club @ UTD",
       location: "Richardson, TX",
-      period: "September 2023 – December 2023",
+      period: "Sep 2023 – Dec 2023",
       description: "Developed an app utilizing deep learning models to detect diabetic retinopathy from retinal images, achieving 95% accuracy in early-stage detection. Designed the app's architecture ensuring seamless integration of deep learning algorithms. Collaborated with medical experts to fine-tune models and improve diagnostic reliability.",
       icon: <Sparkles className="w-6 h-6" />
     }
